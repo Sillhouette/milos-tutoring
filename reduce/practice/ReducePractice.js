008fd83af3586b2f8606e578e810e74d1e8aad25
Austin Melchior <raustin.melchior@gmail.com>
2021-01-16 00:31:41 -0800

    add ReducePractice.js with prompts and structure

---
+++ reduce/practice/ReducePractice.js
@@ -0,0 +1,49 @@
+// 1. Create a function that takes an array of numbers and totals them
+function total(numbers) {
+
+}
+
+// 2. Create a function that takes an array of numbers and multiplies them
+function product(numbers) {
+
+}
+
+// 3. Create a function that takes an array of strings and concatenates them
+function concatString(items) {
+
+}
+
+// 4. Create a function that takes an array of voter objects and totals the
+//    number of people who have already voted
+function totalVotes(voters) {
+
+}
+
+// 5. Create a function that takes an array of wishlist items and totals the prices
+function shoppingSpree(wishlist) {
+
+}
+
+// 6. Create a function that takes an array of arrays and joins them into a single array
+// Note: Take a look at Array.concat() to help with this one
+function flatten(nestedArrays) {
+
+}
+
+// 7. Given an array of potential voters, return an object representing the results of the vote
+//    Include how many of the potential voters were in the ages 18-25, how many from 26-35, 
+//    how many from 36-55, and how many of each of those age ranges actually voted. 
+//    The resulting object containing this data should have 6 properties. 
+function voterResults(voters) {
+    
+ }
+
+module.exports = {
+    total,
+    product,
+    concatString,
+    totalVotes,
+    shoppingSpree,
+    flatten,
+    voterResults
+}
